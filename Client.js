8bb767d9d0af2c8151bf4a1922afd30e53ede771
Loginworks Softwares <38474202+amitpmloginworks@users.noreply.github.com>
2019-01-21 12:30:10 +0530

    update

--- Client.js
+++ Client.js
@@ -117,6 +117,33 @@ client.on('data', function (data) {
   });
 
 });
+  socket_port=PORT
+try_other_port = function() {
+
+  if (socket_port !== 80) {
+    if (typeof console !== "undefined" && console !== null) {
+      console.log("Trying other port, instead of port " + socket_port + ", attempting port 80");
+    }
+    socket_port = 80;
+    client.options.port = socket_port;
+    client.options.transports = ['htmlfile', 'xhr-multipart', 'xhr-polling', 'jsonp-polling'];
+    return socket.client();
+  } else {
+    return typeof console !== "undefined" && console !== null ? console.log("No other ports to try.") : void 0;
+  }
+};
+client.on('connect_failed', function() {
+  if (typeof console !== "undefined" && console !== null) {
+    console.log("Connect failed (port " + socket_port + ")");
+  }
+  return try_other_port();
+});
+client.on('error', function() {
+  if (typeof console !== "undefined" && console !== null) {
+    console.log("Socket.io reported a generic error");
+  }
+  return try_other_port();
+});
 }
 
 io.on('connection', (socket) => {
